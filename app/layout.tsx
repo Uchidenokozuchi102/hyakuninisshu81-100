786e8ccc67a637435b850a2b1e8a03188df6d6d9
Teacher <teacher@example.com>
2025-12-10 12:26:35 +0900

    fix open graph metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,13 +1,29 @@
+// app/layout.tsx
+
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
 
 const inter = Inter({ subsets: ['latin'] })
 
+// ↓↓↓ ここを修正 ↓↓↓
 export const metadata: Metadata = {
   title: '百人一首 App',
   description: '百人一首 No.81-100',
+  openGraph: {
+    title: '百人一首 App',
+    description: '百人一首 No.81-100',
+    siteName: '百人一首 App',
+    locale: 'ja_JP',
+    type: 'website',
+  },
+  twitter: {
+    card: 'summary',
+    title: '百人一首 App',
+    description: '百人一首 No.81-100',
+  },
 }
+// ↑↑↑ ここまで ↑↑↑
 
 export default function RootLayout({
   children,
@@ -16,13 +32,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="ja">
-      <head>
-        <meta name="google" content="notranslate" />
-      </head>
-      {/* 以前のエラーの原因だったGeistやAnalyticsはここから排除されています */}
-      <body className={inter.className}>
-        {children}
-      </body>
+      <body className={inter.className}>{children}</body>
     </html>
   )
 }
